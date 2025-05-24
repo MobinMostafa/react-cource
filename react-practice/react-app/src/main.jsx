5a9bbe88a5bca2da561e6622f71d568359804580
MOBIN MOSTAFA <mobinmostafa920@gmail.com>
2025-05-24 12:33:23 +0600

    react-router add 02

--- react-practice/react-app/src/main.jsx
+++ react-practice/react-app/src/main.jsx
@@ -1,10 +1,37 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Layout from './pages/Layout.jsx';
+import Home from './pages/Home.jsx';
+import About from './pages/About.jsx';
+import NoPage from './pages/NoPage.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+      {
+        path: "*",
+        element: <NoPage />,
+      },
+    ],  
+  },
+
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+   <RouterProvider router={router} />
   </StrictMode>,
 )
